1f6c3db99fb22fff228ca8831ceb9d4e5b339921
nmartinelli23 <natasha.martinelli@wolox.com.ar>
2018-10-04 09:28:51 -0300

    form layout and configuration

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -10,7 +12,9 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
