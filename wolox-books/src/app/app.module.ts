86e426f52b82b40584374b1fdc70b2938b9a15e6
nmartinelli23 <natasha.martinelli@wolox.com.ar>
2018-10-25 09:45:57 -0300

    changes of sass and comment remove

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { Router } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { RegisterComponent } from './screens/register/register.component';
 import { LoginComponent } from './screens/login/login.component';
 import { AuthComponent } from './screens/auth/auth.component';
