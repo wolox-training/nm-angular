36b51b72aa97b00e55acaaa8b7c169c503077eb5
nmartinelli23 <natasha.martinelli@wolox.com.ar>
2018-12-28 14:00:45 -0300

    login view

--- wolox-books/src/views/Login.vue
+++ wolox-books/src/views/Login.vue
@@ -1,7 +1,61 @@
 <template lang='pug'>
+.container
+  form.form-container(@submit.prevent='onSubmit' novalidate)
+    img.wolox-image(src='../assets/wolox.png' alt='Wolox')
+    .input-container
+      label.input-label
+        | Email
+      input.input-field(type='email' v-model='email')
+      span.errorMessage(v-if='$v.email.$invalid && submitted')
+        | Invalid field.
+    .input-container
+      label.input-label
+        | Password
+      input.input-field(type='password' v-model='password')
+      span.errorMessage(v-if='$v.password.$invalid && submitted')
+        | Invalid field. Upper letters and numbers are required.
+    button.green-button(type='submit')
+      | Log in
+    routerLink(to='/signup')
+      button.grey-button(type='button')
+        | Sign up
 </template>
 
 <script>
+import { required, email } from 'vuelidate/lib/validators'
+import validatePassword from '../config/helpers'
+
+export default {
+  name: 'register',
+  data () {
+    return {
+      email: null,
+      password: null,
+      locale: 'en',
+      submitted: false
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.submitted = true
+      if (!this.$v.$invalid) {
+        const userData = {
+          email: this.email,
+          password: this.password,
+          locale: 'en'
+        }
+        console.log(userData)
+        this.email = null
+        this.password = null
+        this.submitted = false
+      }
+    }
+  },
+  validations: {
+    email: { required, email },
+    password: { required, validatePassword }
+  }
+}
 </script>
 
 <style scoped lang='scss'>
