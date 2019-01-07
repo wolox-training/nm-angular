226b43821f3a4ef1ede9f56609287f2c7dd82886
nmartinelli23 <natasha.martinelli@wolox.com.ar>
2019-01-07 16:20:32 -0300

    replace components for views

--- wolox-books/src/views/Dashboard.vue
+++ wolox-books/src/views/Dashboard.vue
@@ -1,15 +1,54 @@
 <template lang='pug'>
-div
-  navbar
-  books-list
+.books-container
+  router-link.books-list(
+    v-for='book in allBooks'
+    :key='book.id'
+    :to="{ name: 'BookDetail', params: { id: book.id }}"
+  )
+    img.book-image(:src='book.image_url' :alt='book.title')
+    span.bold.text-ending
+      | {{book.title}}
+    span.text-ending
+      | {{book.author}}
 </template>
 
 <script>
-import Navbar from '@/components/Navbar'
-import BooksList from '@/components/BooksList'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
-  name: 'dashboard',
-  components: { Navbar, BooksList }
+  methods: {
+    ...mapActions(['bringBooks']),
+    getBooks() {
+      this.bringBooks()
+    }
+  },
+  computed: {
+    ...mapGetters(['allBooks'])
+  },
+  beforeMount() {
+    this.getBooks()
+  }
 }
 </script>
+
+<style scoped lang="scss">
+@import '../scss/variables/colors';
+
+.books-container {
+  display: grid;
+  grid-auto-rows: 250px;
+  grid-gap: 15px;
+  grid-template-columns: repeat(4, 200px);
+  max-width: 800px;
+  margin: 30px auto;
+  width: 100%;
+}
+
+.books-list {
+  padding: 20px;
+  background: $light-gray;
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
